7979761611575f703d3f7145feb6415882175924
NabilaAzzahrra <zahrrranabill11@gmail.com>
2024-05-06 11:35:10 +0700

    warna judul

--- src/pages/Hasil.jsx
+++ src/pages/Hasil.jsx
@@ -85,7 +85,7 @@ const Hasil = () => {
                     {result.keterangan.split(/([:.])/).map((sentence, index, array) => (
                       <span key={index}>
                         {index % 4 === 0 ? (
-                          <b className='text-amber-400'>{sentence.trim()}</b>
+                          <b className='text-red-500'>{sentence.trim()}</b>
                         ) : (
                           <span>{sentence.trim()}</span>
                         )}
