7b026412f0b84fcb9a5aa235abe7d73b51e8331a
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-05-08 10:18:44 +0700

    focus

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -187,35 +187,35 @@ function Question() {
                     <div className='flex flex-col md:flex-row md:items-center justify-center gap-5 md:gap-10 p-4'>
                         <label>
                             <input type="radio" name="option" value="4" onClick={handleOptionSelect} className='hidden' checked={selectedOption === 1} />
-                            <div className='flex text-[15px] justify-center bg-white px-4 py-2 cursor-pointer flex items-center gap-2 rounded-full border-2 sm:hover:bg-black sm:hover:text-white border-black'>
+                            <div className='flex text-[15px] justify-center bg-white px-4 py-2 cursor-pointer flex items-center gap-2 rounded-full border-2 focus:bg-black focus:text-white border-black'>
                                 Gue Banget
                                 <div className='text-xl rounded-full'>👍</div>
                             </div>
                         </label>
                         <label>
                             <input type="radio" name="option" value="3" onClick={handleOptionSelect} className='hidden' checked={selectedOption === 2} />
-                            <div className='flex text-[15px] justify-center bg-white px-4 py-2 cursor-pointer flex items-center gap-2 rounded-full border-2 sm:hover:bg-black sm:hover:text-white border-black'>
+                            <div className='flex text-[15px] justify-center bg-white px-4 py-2 cursor-pointer flex items-center gap-2 rounded-full border-2 focus:bg-black focus:text-white border-black'>
                                 Pas di Gue Sih
                                 <div className='text-xl rounded-full'>👌</div>
                             </div>
                         </label>
                         <label>
                             <input type="radio" name="option" value="2" onClick={handleOptionSelect} className='hidden' checked={selectedOption === 3} />
-                            <div className='flex text-[15px] justify-center bg-white px-4 py-2 cursor-pointer flex items-center gap-2 rounded-full border-2 sm:hover:bg-black sm:hover:text-white border-black'>
+                            <div className='flex text-[15px] justify-center bg-white px-4 py-2 cursor-pointer flex items-center gap-2 rounded-full border-2 focus:bg-black focus:text-white border-black'>
                                 Bukan Gue
                                 <div className='text-xl rounded-full'>👋</div>
                             </div>
                         </label>
                         <label>
                             <input type="radio" name="option" value="1" onClick={handleOptionSelect} className='hidden' checked={selectedOption === 4} />
-                            <div className='flex text-[15px] justify-center bg-white px-4 py-2 cursor-pointer flex items-center gap-2 rounded-full border-2 sm:hover:bg-black sm:hover:text-white border-black'>
+                            <div className='flex text-[15px] justify-center bg-white px-4 py-2 cursor-pointer flex items-center gap-2 rounded-full border-2 focus:bg-black focus:text-white border-black'>
                                 Bukan Gue Banget
                                 <div className='text-xl rounded-full'>👎</div>
                             </div>
                         </label>
                     </div>
                     <div>
-                        <p className='text-xs text-center px-6 xl:text-md'>© A Chapman and V Chislett MSc 2005, diambil dari <span className='italic'>Gardner's Multiple Intelligences Model</span>. From <span className='italic'>www.businessballs.com</span> dengan item yang telah dimodifikasis. Tidak untuk dijual dan dicetak. Penulis tidak bertanggung-jawab atas pelanggaran hal-hal tersebut.</p>
+                        <p className='text-xs text-center px-6 xl:text-md'>© A Chapman and V Chislett MSc 2005, diambil dari <span className='italic'>Gardner's Multiple Intelligences Model</span>. From <span className='italic'>www.businessballs.com</span> dengan item yang telah dimodifikasi. Tidak untuk dijual dan dicetak. Penulis tidak bertanggung-jawab atas pelanggaran hal-hal tersebut.</p>
                     </div>
                 </div>
             </section>
